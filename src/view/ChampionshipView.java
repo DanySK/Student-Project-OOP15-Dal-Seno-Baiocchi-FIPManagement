d84ef97078b5da5d4b213bbfa54181f775a7893d
lucaDalSeno <lucaDalSeno@localhost>
2016-09-05 17:00:30 +0200

    Set sizable

--- src/view/ChampionshipView.java
+++ src/view/ChampionshipView.java
@@ -3,6 +3,8 @@ package view;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
